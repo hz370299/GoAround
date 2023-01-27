76daf9cc47c447e5ac20bb33d05e480098aa1a2f
Zheng Han <hanzheng@seas.upenn.edu>
2023-01-26 22:33:38 -0500

    search function

---
+++ src/around/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://localhost:9200/"
+	ES_USERNAME = "hz"
+	ES_PASSWORD = "hz"
+)
--- src/around/handler/post.go
+++ src/around/handler/post.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"around/model"
+	"around/service"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +18,37 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Post Received: %s\n", p.Message)
+}
+
+// handle search-related requests
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	w.Header().Set("Content-Type", "application/json")
+
+	user := r.URL.Query().Get("user")
+	keywords := r.URL.Query().Get("keywords")
+
+	var posts []model.Post
+	var err error
+
+	if user != "" {
+		posts, err = service.SearchPostsByUser(user)
+	} else {
+		posts, err = service.SearchPostsByKeywords(keywords)
+	}
+
+	if err != nil {
+		http.Error(w, "Failed to read post from backend", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from backend %v. \n", err)
+		return
+	}
+
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v. \n", err)
+		return
+	}
+
+	w.Write(js)
 }
